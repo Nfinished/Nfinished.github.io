a53d0f1ff568a63cea312efe944513f700246f5b
Adam Trager <unonymous253@gmail.com>
2016-02-22 20:09:37 -0500

    example for glitterati

--- js/blow.js
+++ js/blow.js
@@ -3,24 +3,24 @@ var defaultPos = '50%';
 
 
 // HELPER FUNCTIONS
-function setTextColor(color) {
-  $("h1").css("color", color);
+function setTextColor(element, color) {
+  $(element).css("color", color);
 }
 
 function setBackgroundColor(color) {
   $("body").css("background-color", color);
 }
 
-function fadeOut(time) {
-  $("h1").fadeOut(time);
+function fadeOut(element, time) {
+  $(element).fadeOut(time);
 }
 
-function setXPos(x) {
-  $("div").css("left", x);
+function setXPos(element, x) {
+  $(element).parent().css("left", x);
 }
 
-function setYPos(y) {
-  $("div").css("top", y);
+function setYPos(element, y) {
+  $(element).parent().css("top", y);
 }
 
 
@@ -28,9 +28,9 @@ function setYPos(y) {
 window.onload = function() {
   for (var i = 0; i < lyrics.length; i++) {
     setTimeout(function(state) {
-    //window.history.replaceState(null, null, state.word);                        //Set page URL
+    //window.history.replaceState(null, null, state.word);                  //Set page URL
     $("title").text(state.word);                                            //Set page title
-    $("h1").show().text(state.word);                                        //Set page content and make sure it's visible
+    $(state.element).show().text(state.word);                                        //Set page content and make sure it's visible
 
     if(state.logging) {                                                     //If there are specifc console settings, use those
       switch(state.logging) {
@@ -45,10 +45,10 @@ window.onload = function() {
     }
 
     if (state.backgroundColor) {setBackgroundColor(state.backgroundColor);} //Set background color
-    if(state.fadeOut) {fadeOut(state.fadeOut);}                             //Set fadeOut
-    if (state.textColor) {setTextColor(state.textColor);}                   //Set text color
-    if (state.X) {setXPos(state.X);} else {setXPos(defaultPos);}            //Set text X position
-    if (state.Y) {setYPos(state.Y);} else {setYPos(defaultPos);}            //Set text Y position
+    if(state.fadeOut) {fadeOut(state.element, state.fadeOut);}                             //Set fadeOut
+    if (state.textColor) {setTextColor(state.element, state.textColor);}                   //Set text color
+    //if (state.X) {setXPos(state.X);} else {setXPos(state.element, defaultPos);}            //Set text X position
+    //if (state.Y) {setYPos(state.Y);} else {setYPos(state.element, defaultPos);}            //Set text Y position
 
   },
     lyrics[i].timestamp,
--- js/lyrics.js
+++ js/lyrics.js
@@ -12,243 +12,321 @@
 var lyrics = [
   {
     fadeOut: 750,
-    textColor: '#fff',
+    element: '.center h1',
     word: 'DANCE',
     timestamp: 3500
   },
   {
+    element: '.center h1',
     word: 'BACK',
     timestamp: 12176
   },
   {
+    element: '.center h1',
     word: 'DOOR',
     timestamp: 12700
   },
   {
+    element: '.center h1',
     word: 'CRACKED',
     timestamp: 13170
   },
   {
+    element: '.center h1',
     word: 'WE',
     timestamp: 13635
   },
   {
+    element: '.center h1',
     word: "DON'T",
     timestamp: 14175
   },
   {
+    element: '.center h1',
     word: "NEED",
     timestamp: 14400
   },
   {
+    element: '.center h1',
     word: "A",
     timestamp: 14605
   },
   {
+    element: '.center h1',
     fadeOut: 500,
     word: "KEY",
     timestamp: 14908
   },
   {
+    element: '.center h1',
     word: "WE",
     timestamp: 15817
   },
   {
+    element: '.center h1',
     word: "GET",
     timestamp: 16181
   },
   {
+    element: '.center h1',
     word: "IN",
     timestamp: 16385
   },
   {
+    element: '.center h1',
     word: "FOR",
     timestamp: 16746
   },
   {
+    element: '.center h1',
     fadeOut: 500,
     word: "FREE",
     timestamp: 16900
   },
   {
+    element: '.center h1',
     word: "NO",
     timestamp: 17746
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "V",
     timestamp: 17900
   },
   {
+    element: '.center h1',
     logging: "VIP",
     word: "VI",
     timestamp: 18100
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "VIP",
     timestamp: 18275
   },
   {
+    element: '.center h1',
     fadeOut: 750,
     word: "SLEAZE",
     timestamp: 18675
   },
   {
+    element: '.center h1',
     word: "DRINK",
     timestamp: 20115
   },
   {
+    element: '.center h1',
     word: "THE",
     timestamp: 20674
   },
   {
+    element: '.center h1',
     word: "KOOL",
     timestamp: 21165
   },
   {
+    element: '.center h1',
     word: "AID",
     timestamp: 21678
   },
   {
+    element: '.center h1',
     word: "FOLLOW",
     timestamp: 22102
   },
   {
+    element: '.center h1',
     word: "MY",
     timestamp: 22350
   },
   {
+    element: '.center h1',
     fadeOut: 500,
     word: "LEAD",
     timestamp: 22650
   },
   {
+    element: '.center h1',
     word: "NOW",
     timestamp: 23845
   },
   {
+    element: '.center h1',
     word: "YOU'RE",
     timestamp: 24000
   },
   {
+    element: '.center h1',
     word: "ONE",
     timestamp: 24375
   },
   {
+    element: '.center h1',
     word: "OF",
     timestamp: 24669
   },
   {
+    element: '.center h1',
     fadeOut: 250,
     word: "US",
     timestamp: 24996
   },
   {
+    element: '.center h1',
     word: "YOU'RE",
     timestamp: 25786
   },
   {
+    element: '.center h1',
     word: "COMIN",
     timestamp: 26000
   },
   {
+    element: '.center h1',
     word: "WITH",
     timestamp: 26500
   },
   {
+    element: '.center h1',
     fadeOut: 250,
     word: "ME",
     timestamp: 26834
   },
   {
+    element: '.center h1',
     logging: "IIIIIIT'S",
     word: "I",
     timestamp: 28225
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "II",
     timestamp: 28325
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "III",
     timestamp: 28375
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "IIII",
     timestamp: 28425
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "IIIII",
     timestamp: 28475
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "IIIIII",
     timestamp: 28525
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "IIIIIIT",
     timestamp: 28575
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "IIIIIIT'",
     timestamp: 28625
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "IIIIIIT'S",
     timestamp: 28675
   },
   {
+    element: '.center h1',
     word: "TIME",
     timestamp: 28931
   },
   {
+    element: '.center h1',
     word: "TO",
     timestamp: 29616
   },
   {
+    element: '.center h1',
     word: "KILL",
     timestamp: 29859
   },
   {
+    element: '.center h1',
     word: "THE",
     timestamp: 30622
   },
   {
+    element: '.center h1',
     word: "LIGHTS",
     timestamp: 30893
   },
   {
+    element: '.center h1',
     word: "AND",
     timestamp: 31471
   },
   {
+    element: '.center h1',
     word: "SHUT",
     timestamp: 31906
   },
   {
+    element: '.center h1',
     word: "THE",
     timestamp: 32705
   },
   {
+    element: '.center h1',
     logging: "none",
     word: "D",
     timestamp: 32967
   },
   {
+    element: '.center h1',
     word: "DJ",
     timestamp: 33375
   },
   {
+    element: '.center h1',
     word: "DOWN",
     timestamp: 33820
   },
+  {
+    element: '.bottomLeft h1',
+    fadeOut: 100,
+    word: "THIS",
+    timestamp: 34954
+  },
+  {
+    element: '.bottomLeftCenter h1',
+    fadeOut: 100,
+    word: "PLACE",
+    timestamp: 35219
+  },
+  {
+    element: '.bottomRightCenter h1',
+    fadeOut: 100,
+    word: "ABOUT",
+    timestamp: 35477
+  },
+  {
+    element: '.bottomRight h1',
+    fadeOut: 100,
+    word: "TO",
+    timestamp: 35800
+  }
 ];
