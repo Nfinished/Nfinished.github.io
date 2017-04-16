6d396f4e755e29bb952f84d960ddfccb837f5c29
Adam Trager <unonymous253@gmail.com>
2017-04-16 14:29:19 -0400

    unfinished.business

--- sandbox/BT/scripts.js
+++
@@ -1,9 +0,0 @@
-(function(window, document, $) {
-  var $label = $('#footer span');
-  $('a').hover(function() {
-    $label.html($(this).attr('data-value'));
-  },
-  function() {
-    $label.html('');
-  });
-}( window, document, jQuery ));
